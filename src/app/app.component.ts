681a57dd3a44f63932c16ad1041f8993362db913
mehdi-mtir <mehdi.mtir@gmail.com>
2023-11-27 14:06:14 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'first App';
+  show = true
+  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png';
+  mois = ["Janvier", "Février", "Mars", "..."]
+  sayHello(){
+    this.show = !this.show;
+  }
 }
